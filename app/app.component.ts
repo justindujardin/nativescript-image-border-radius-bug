88910845a24fbec237acce6e1635941f70978d3f
Justin DuJardin <justin@dujardinconsulting.com>
2017-09-05 09:09:52 -0700

    chore: app component shows ios image border radius bug

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "app.component.html",
+    styles: [`
+      Image { 
+          border-width: 1;
+          border-color: green;
+      }
+    `]
+})
+
+export class AppComponent { }
